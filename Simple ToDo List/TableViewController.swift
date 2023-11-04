e990eb8c2e9a66fb4a79ee360736d8258e260058
vyatkinsen <vyatkinsen@bk.ru>
2023-11-04 16:37:11 +0300

    Initial commit

---
+++ Simple ToDo List/TableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TableViewController.swift
+//  Simple ToDo List
+//
+//  Created by Арсений Вяткин on 04.11.2023.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    // MARK: - Properties
+    var tasks: [String] = []
+
+    // MARK: - IBActions
+    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "New task", message: "Please add a new task", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            let textField = alertController.textFields?.first
+            if let newTask = textField?.text {
+                self.tasks.insert(newTask, at: 0)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        alertController.addTextField { _ in }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
