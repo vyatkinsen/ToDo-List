76f580a004412299e2a3b7a1c41e11794f4c94e5
vyatkinsen <vyatkinsen@bk.ru>
2023-11-04 16:14:31 +0300

    Initial Commit

---
+++ Simple ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple ToDo List
+//
+//  Created by Арсений Вяткин on 04.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
